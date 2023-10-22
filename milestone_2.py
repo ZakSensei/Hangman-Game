a170fdb5fa5bbab3fc531dabca97d0f0a1af78f6
Zak Ahmed <sakariye192@gmail.com>
2023-10-22 17:02:07 +0100

    refactor: encapsulating guess in one function

--- milestone_2.py
+++ milestone_2.py
@@ -1,14 +1,16 @@
 import random
-
-word_list = ["Strawberry", "Watermelon", "Apple", "Berries", "Pears"]
-#print(word_list)
+from all_words import word_list 
 
 word = random.choice(word_list)
-#print(word)
 
-guess = input("Enter a single letter: ")
+def user_guess():
 
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
-else:
-    print("Oops! That is not a vaild input.")
+    guess = input("Enter a single letter: ")
+
+    if len(guess) == 1 and guess.isalpha():
+        print("Good guess!")
+    else:
+        print("Oops! That is not a vaild input.")
+
+
+user_guess()
